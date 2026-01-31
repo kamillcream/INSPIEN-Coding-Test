e66de28e7c3786d98732b382d88958d67a62f309
Jinyoung <glitt5384@naver.com>
2026-01-31 22:10:02 +0900

    feat: application 모듈 내 OrderHeader 객체

---
+++ modules-api/application/src/main/java/com/inspien/application/port/in/OrderHeader.java
@@ -0,0 +1,18 @@
+package com.inspien.application.port.in;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderHeader {
+    private String userId;
+
+    private String name;
+
+    private String address;
+
+    private String status;
+}
