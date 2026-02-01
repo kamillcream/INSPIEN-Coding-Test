372cd1750b15437e0796fa10406e28827b84c1be
Jinyoung <glitt5384@naver.com>
2026-02-02 01:25:56 +0900

    feat: 주문 생성 시 필요한 요청 DTO

---
+++ modules-api/bootstrap/src/main/java/com/inspien/api/dto/OrderRequestDto.java
@@ -0,0 +1,37 @@
+package com.inspien.api.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.inspien.application.port.in.OrderCreateCommand;
+import com.inspien.application.port.in.OrderHeader;
+import com.inspien.application.port.in.OrderItem;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JacksonXmlRootElement(localName = "ORDER")
+public class OrderRequestDto {
+
+    @JacksonXmlProperty(localName = "HEADER")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<OrderHeaderDto> headers;
+
+    @JacksonXmlProperty(localName = "ITEM")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<OrderItemDto> items;
+
+    public OrderCreateCommand toCommand(){
+        List<OrderHeader> orderHeaders = this.headers
+                .stream().map(OrderHeaderDto::to).toList();
+
+        List<OrderItem> orderItems = this.items
+                .stream().map(OrderItemDto::to).toList();
+
+        return new OrderCreateCommand(
+                orderHeaders, orderItems
+        );
+    }
+}
+
