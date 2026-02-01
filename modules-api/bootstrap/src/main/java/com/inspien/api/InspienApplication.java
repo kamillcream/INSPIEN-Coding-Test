14576c8405a0827a65fd0a40dcd4051161edda78
Jinyoung <glitt5384@naver.com>
2026-02-02 01:23:12 +0900

    feat: 패키지 스캔 범위 명시

--- modules-api/bootstrap/src/main/java/com/inspien/api/InspienApplication.java
+++ modules-api/bootstrap/src/main/java/com/inspien/api/InspienApplication.java
@@ -2,8 +2,12 @@ package com.inspien.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.inspien"})
+@EnableJpaRepositories(basePackages = {"com.inspien.**.persistence.repository"})
+@EntityScan(basePackages = {"com.inspien.infrastructure.persistence.entity"})
 public class InspienApplication {
 
 	public static void main(String[] args) {
