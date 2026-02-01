efedad66ecf032490c61bcdeaf90c197d3443255
Jinyoung <glitt5384@naver.com>
2026-02-02 01:25:43 +0900

    feat: save 기능 구현 어댑터

---
+++ modules-api/infrastructure/src/main/java/com/inspien/infrastructure/adapter/OrderPersistenceAdapter.java
@@ -0,0 +1,22 @@
+package com.inspien.infrastructure.adapter;
+
+import com.inspien.application.port.out.OrderOutPort;
+import com.inspien.domain.Order;
+import com.inspien.infrastructure.mapper.OrderMapper;
+import com.inspien.infrastructure.persistence.entity.OrderEntity;
+import com.inspien.infrastructure.persistence.repository.OrderJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OrderPersistenceAdapter implements OrderOutPort {
+    private final OrderJpaRepository repo;
+    private final OrderMapper mapper;
+
+    @Override
+    public void save(Order order) {
+        OrderEntity entity = mapper.toEntity(order);
+        repo.save(entity);
+    }
+}
