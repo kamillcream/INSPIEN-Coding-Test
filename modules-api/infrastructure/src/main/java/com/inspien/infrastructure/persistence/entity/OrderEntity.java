ce87f6736b48c52e7b0674e4884f903a85243a75
Jinyoung <glitt5384@naver.com>
2026-02-02 01:24:07 +0900

    feat: orderentity에 생성자 및 빌더 어노테이션 추가

--- modules-api/infrastructure/src/main/java/com/inspien/infrastructure/persistence/entity/OrderEntity.java
+++ modules-api/infrastructure/src/main/java/com/inspien/infrastructure/persistence/entity/OrderEntity.java
@@ -3,10 +3,18 @@ package com.inspien.infrastructure.persistence.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "orders")
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderEntity {
     @Id
     @Column(name = "ORDER_ID")
