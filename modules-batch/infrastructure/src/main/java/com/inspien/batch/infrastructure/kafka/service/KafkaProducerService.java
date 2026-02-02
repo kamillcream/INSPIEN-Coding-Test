357eed53a41a050486117a2300004be8c20b03c8
Jinyoung <glitt5384@naver.com>
2026-02-02 22:09:31 +0900

    feat: 카프카 이벤트 생성 서비스 구현체

---
+++ modules-batch/infrastructure/src/main/java/com/inspien/batch/infrastructure/kafka/service/KafkaProducerService.java
@@ -0,0 +1,19 @@
+package com.inspien.batch.infrastructure.kafka.service;
+
+import com.inspien.batch.application.port.in.KafkaProducerUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducerService implements KafkaProducerUseCase {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendEvent(String orderId) {
+        kafkaTemplate.send("shipment.success", orderId);
+    }
+}
