13d186e507d153b3ba29113d7f1fd4a81cc8ee1d
Jinyoung <glitt5384@naver.com>
2026-02-02 22:13:38 +0900

    feat: Order 상태변경 및 status가 N인 Order 조회하는 인터페이스 추가

--- modules-api/application/src/main/java/com/inspien/application/port/out/OrderOutPort.java
+++ modules-api/application/src/main/java/com/inspien/application/port/out/OrderOutPort.java
@@ -2,7 +2,11 @@ package com.inspien.application.port.out;
 
 import com.inspien.domain.Order;
 
+import java.util.List;
+
 public interface OrderOutPort {
     void save(Order order);
     String findLastId();
+    void markOrderSuccess(String orderId);
+    List<Order> findShipmentCandidates();
 }
