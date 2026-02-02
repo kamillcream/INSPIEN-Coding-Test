096b88aadc9d92bab4c8e7f37a2e9f966bff91ce
Jinyoung <glitt5384@naver.com>
2026-02-02 22:10:48 +0900

    feat: 스케줄러에서 발행한 이벤트를 소비하는 컨슈머

---
+++ modules-batch/infrastructure/src/main/java/com/inspien/batch/infrastructure/kafka/consumer/ShipmentConsumer.java
@@ -0,0 +1,26 @@
+package com.inspien.batch.infrastructure.kafka.consumer;
+
+import com.inspien.batch.application.port.in.ShipmentCommandUseCase;
+import com.inspien.batch.application.port.in.dto.OrderShipmentCommand;
+import com.inspien.batch.infrastructure.kafka.dto.OrderShipmentPayload;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ShipmentConsumer {
+    private final ShipmentCommandUseCase shipmentCommandService;
+
+    @KafkaListener(topics = "shipment.batch", groupId = "shipment-processor", containerFactory = "orderShipmentKafkaListenerContainerFactory")
+    public void consumeBatchEvent(@Payload OrderShipmentPayload payload) {
+        shipmentCommandService.processShipment(OrderShipmentCommand.create(
+                payload.getOrderId(), payload.getItemId(), payload.getApplicantKey(), payload.getAddress()
+        ));
+    }
+}
+
