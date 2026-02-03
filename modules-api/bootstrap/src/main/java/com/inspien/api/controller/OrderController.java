d45bf4c83c82e851384a0e2887ea0b19c1e684e1
Jinyoung <glitt5384@naver.com>
2026-02-03 19:32:08 +0900

    feat: 로직 성공적으로 처리시 응답에 String 반환

--- modules-api/bootstrap/src/main/java/com/inspien/api/controller/OrderController.java
+++ modules-api/bootstrap/src/main/java/com/inspien/api/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.inspien.api.dto.OrderRequestDto;
 import com.inspien.api.application.port.in.OrderCommandUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,8 @@ public class OrderController {
     @PostMapping(
             consumes = MediaType.APPLICATION_XML_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public void createOrder(@Validated @RequestBody OrderRequestDto requestDto) {
+    public ResponseEntity<String> createOrder(@Validated @RequestBody OrderRequestDto requestDto) {
         orderCommandService.processOrder(requestDto.toCommand());
+        return ResponseEntity.ok("성공");
     }
 }
