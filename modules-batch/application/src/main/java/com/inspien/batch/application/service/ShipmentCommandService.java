cd595433b6fc96ff75149819e621f11c27322481
Jinyoung <glitt5384@naver.com>
2026-02-03 15:02:55 +0900

    feat: log 추가

--- modules-batch/application/src/main/java/com/inspien/batch/application/service/ShipmentCommandService.java
+++ modules-batch/application/src/main/java/com/inspien/batch/application/service/ShipmentCommandService.java
@@ -1,12 +1,12 @@
 package com.inspien.batch.application.service;
 
-import com.inspien.batch.application.port.in.KafkaProducerUseCase;
 import com.inspien.batch.application.port.in.ShipmentCommandUseCase;
 import com.inspien.batch.application.port.in.dto.OrderShipmentCommand;
 import com.inspien.batch.application.port.in.dto.ShipmentCreatedEvent;
 import com.inspien.batch.application.port.out.ShipmentOutPort;
 import com.inspien.batch.domain.Shipment;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = false)
+@Slf4j
 public class ShipmentCommandService implements ShipmentCommandUseCase {
     private final ShipmentOutPort repo;
     private final ApplicationEventPublisher eventPublisher;
@@ -24,6 +25,7 @@ public class ShipmentCommandService implements ShipmentCommandUseCase {
                 command.getApplicantKey(), command.getAddress());
 
         repo.save(shipment);
+        log.info("[SHIPMENT_SAVE] shipmentId={} step=SUCCESS", shipment.getShipmentId());
 
         eventPublisher.publishEvent(new ShipmentCreatedEvent(command.getOrderId()));
     }
@@ -45,6 +47,10 @@ public class ShipmentCommandService implements ShipmentCommandUseCase {
             prefix++;
 
             if (prefix > 'Z') {
+                log.error(
+                        "[SHIPMENT_SAVE] step=ID_GENERATE_FAIL shipmentId={} reason=ID_RANGE_OVER",
+                        prefix
+                );
                 throw new IllegalStateException("배송 ID 최대 범위를 초과했습니다.");
             }
         }
