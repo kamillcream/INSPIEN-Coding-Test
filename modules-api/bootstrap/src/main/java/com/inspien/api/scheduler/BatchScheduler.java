219db56bdb7946ea749b937df15d2871ce052aa2
Jinyoung <glitt5384@naver.com>
2026-02-03 11:26:31 +0900

    fix: 스케줄러 내에서 트랜잭션을 찾는 에러 해결

--- modules-api/bootstrap/src/main/java/com/inspien/api/scheduler/BatchScheduler.java
+++ modules-api/bootstrap/src/main/java/com/inspien/api/scheduler/BatchScheduler.java
@@ -1,37 +1,21 @@
 package com.inspien.api.scheduler;
 
-import com.inspien.application.port.in.KafkaProducerUseCase;
-import com.inspien.application.port.in.OrderShipmentPayload;
-import com.inspien.application.port.out.OrderOutPort;
-import com.inspien.domain.Order;
+import com.inspien.application.port.in.SchedulerUseCase;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 
 @Component
 @RequiredArgsConstructor
 public class BatchScheduler {
-    private final OrderOutPort orderRepo;
-    private final KafkaProducerUseCase kafkaProducerService;
+    private final SchedulerUseCase schedulerService;
 
     // TODO: 5분으로 변경
     @Scheduled(cron = "*/5 * * * * *")
     public void publishShipmentReadyEvents() {
-        List<Order> orders = orderRepo.findShipmentCandidates();
-        List<OrderShipmentPayload> payloads = convertToPayload(orders);
-        payloads.forEach(kafkaProducerService::sendEvent);
-    }
-
-    private List<OrderShipmentPayload> convertToPayload(List<Order> entities) {
-        if(entities.isEmpty()) {
-            return List.of();
-        }
-        return entities.stream()
-                .map(domain -> OrderShipmentPayload.create(domain.getOrderId(), domain.getItemId(),
-                        domain.getApplicantKey(), domain.getAddress()))
-                .toList();
+        schedulerService.publishShipmentReadyEvents();
     }
 }
